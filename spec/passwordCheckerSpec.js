c2dbecf5fdfc181088637cb624c26d9ba0dc99a8
Nkosi97 <nkosinathisiyeni97@gmail.com>
2020-04-03 10:37:31 +0200

    first commit

---
+++ spec/passwordCheckerSpec.js
@@ -0,0 +1,46 @@
+let myTest = require("../src/password_Checker");
+let passwordIsOkay =  myTest.passwordIsOkay()
+let password_is_valid = myTest.password_is_valid()
+
+//password_is_valid
+describe("password is valid function", () => {
+  it(`should return password is Valid`, () => {
+    expect(password_is_valid("")).toBe(`password shoud exist`)
+  });
+  it(`should return password is Valid`, () => {
+    expect(password_is_valid("2")).toBe(`password shoud have eight characters`)
+  });
+  it(`should return password is Valid`, () => {
+    expect(password_is_valid("lkjuhgyf")).toBe(`password shoud atleast have one upper character`)
+  });
+  it(`should return password is Valid`, () => {
+    expect(password_is_valid("l*dobeatS")).toBe(`password shoud atleast have one special character`)
+  });
+  it(`should return password is Valid`, () => {
+    expect(password_is_valid("Lavis@97")).toBe(`password is valid!`)
+  });
+ 
+
+});
+
+//passwordIsOkay
+describe("password is ok function", () => {
+  it(`should return password is Ok`, () => {
+    expect(passwordIsOkay("")).toThrow("password is not okay!");
+  });
+  it(`should return password is ok`, () => {
+    expect(passwordIsOkay("s")).toThrow("password is not okay!");
+  });
+  it(`should return password is ok`, () => {
+    expect(passwordIsOkay("laidobeats")).toThrow("password is not okay!");
+  });
+  it(`should return password is ok`, () => {
+    expect(passwordIsOkay("Laidobeats")).toThrow("password is not okay!");
+  });
+  it(`should return password is ok`, () => {
+    expect(passwordIsOkay("L@idobeats")).toThrow("password is not okay!");
+  });
+  it(`should return password is `, () => {
+    expect(passwordIsOkay("L@id0beats97")).toBe("password is okay!")
+  });
+});
---
+++ src/password_Checker.js
@@ -0,0 +1,41 @@
+function password_is_valid(password) {
+
+    try {
+        if (password == "") throw `password should exist`
+        if (password.length < 8) throw `password should atleast have 8 characters`
+        if (password.match(/[a-z]/g) == null) throw `password should have one lower character`
+        if (password.match(/[A-Z]/g) == null) throw `password should have uppercase letters`
+        if (password.match(/[{(!@#$%^&*.,\')}]/g) == null) throw `password should have atleast one special character`
+        if (password.match(/[0-9]/g) == null) throw `password should have have atleast one number`
+
+        return `password is valid!`;
+    } catch (error) {
+        console.error(`Invalid password: ${error}`)
+    }
+    return password
+
+}
+
+// console.log(password_is_valid("L@97"));
+
+function passwordIsOk(password) {
+
+    try {
+        if (password == "") throw `password is not okay!`
+        if (password.length < 8) throw `password is not okay!`
+        if (password.match(/[a-z]/g) == null) throw `password is not okay!`
+        if (password.match(/[A-Z]/g) == null) throw `password is not okay!`
+        if (password.match(/[{(!@#$%^&*.,\')}]/g) == null) throw `password is not okay!`
+        if (password.match(/[0-9]/g) == null) throw `password is not okay!`
+
+        return `password is okay!`;
+    } catch (error) {
+        console.error(`${error}`)
+    }
+    return password
+ }
+
+console.log(passwordIsOk("laido"));
+
+module.exports = { password_is_valid, passwordIsOk}
+   
