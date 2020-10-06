31adb482a81fbdbd9ba44ae19c82887147a504eb
Nkosi97 <nkosinathisiyeni97@gmail.com>
2020-10-06 12:38:17 +0200

    fixed src

--- src/password_Checker.js
+++ src/password_Checker.js
@@ -1,31 +1,37 @@
-function password_is_valid(password) {
+// function password_is_valid(password) {
 
-    try {
-        if (password == "") throw `password should exist`
-        if (password.length < 8) throw `password should atleast have 8 characters`
-        if (!password.match(/[a-z]/g)) throw `password should have one lower character`
-        if (!password.match(/[A-Z]/g)) throw `password should have uppercase letters`
-        if (!password.match(/[{(!@#$%^&*.,\')}]/g)) throw `password should have atleast one special character`
-        if (!password.match(/[0-9]/g)) throw `password should have atleast one number`
+//     try {
+//         if (password == "") throw `password should exist`
+//         if (password.length < 8) throw `password should atleast have 8 characters`
+//         if (!password.match(/[a-z]/g)) throw `password should have one lower character`
+//         if (!password.match(/[A-Z]/g)) throw `password should have uppercase letters`
+//         if (!password.match(/[{(!@#$%^&*.,\')}]/g)) throw `password should have atleast one special character`
+//         if (!password.match(/[0-9]/g)) throw `password should have atleast one number`
 
-        return `password is valid!`;
-    } catch (error) {
-        console.error(`Invalid password: ${error}`)
-    }
-    return password
+//         return `password is valid!`;
+//     } catch (error) {
+//         console.error(`Invalid password: ${error}`)
+//     }
+//     return password
 
-}
+// }
 
 function passwordIsOk(password) {
-        if(password !== "" && password.length > 8 && password.match(/[a-z]/g)){
+   const passArr = [!password.match(/[A-Z]/g), password.length < 8, !password.match(/[a-z]/g)];
+   for(let i = 0; i < passArr.length; i++){
+       if(passArr[i] == password){
+           return true;
+       }else if(passArr[1] || passArr[2] == password){
         return true;
-        }else if(password == "" && password.length < 8){
-         return `password is never OK`;
-        }
-        return false;
+       }
+       return false;
+   }
  }
+ console.log(passwordIsOk("LAAAAAAAAKI"));
+ console.log(passwordIsOk("olo"));
+ console.log(passwordIsOk("Ljhhuin@a"));
 
-console.log(passwordIsOk("laidobeats"));
 
-module.exports = {password_is_valid, passwordIsOk}
+
+// module.exports = {password_is_valid, passwordIsOk}
    
