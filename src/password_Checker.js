ce46ab4dfe6f752be4cbbb699c15682dd7d8a264
Nkosi97 <nkosinathisiyeni97@gmail.com>
2021-02-02 13:36:43 +0200

    change function passwordIsOk to passwordStrength and modified the code

--- src/password_Checker.js
+++ src/password_Checker.js
@@ -1,38 +1,59 @@
 function passwordIsValid(password) {
+  try {
+    if (password == "") throw `password should exist`;
+    if (password.length < 8) throw `password should at least have 8 characters`;
+    if (!password.match(/[a-z]/g))
+      throw `password should have one lower character`;
+    if (!password.match(/[A-Z]/g))
+      throw `password should have uppercase letters`;
+    if (!password.match(/\W/g))
+      throw `password should have atleast one special character`;
+    if (!password.match(/[0-9]/g))
+      throw `password should have atleast one number`;
 
-    try {
-        if (password == "") throw `password should exist`
-        if (password.length < 8) throw `password should at least have 8 characters`
-        if (!password.match(/[a-z]/g)) throw `password should have one lower character`
-        if (!password.match(/[A-Z]/g)) throw `password should have uppercase letters`
-        if (!password.match(/[{(!@#$%^&*.,\')}]/g)) throw `password should have atleast one special character`
-        if (!password.match(/[0-9]/g)) throw `password should have atleast one number`
-
-        return `password is valid!`;
-    } catch (error) {
-        console.error(`Invalid password: ${error}`)
-    }
-    return password
+    return `password is valid!`;
+  } catch (error) {
+    console.error(`Invalid password: ${error}`);
+  }
+  return password;
 }
 
-function passwordIsOk(password) {
-   var conditions = 0 
-   const passArr = 
-   [password !== "",password.match(/[A-Z]/g),
-    password.length > 8, password.match(/[a-z]/g),
-    password.match(/\d/g),password.match(/\W/g)];
-   for(let i = 0; i < passArr.length; i++){
-       if(passArr[i]){
-            conditions++
-       }if(passArr[0] && passArr[2] !== true){
-        return `password is never OK`;
-       }
-        if(conditions >= 3){
-            return true
-        }
-   }
-   return false;
- }
+function passwordStrength(password) {
+  let conditions = 0;
 
-module.exports = {passwordIsValid, passwordIsOk}
-   
+  if (password != "") {
+    conditions = conditions + 1;
+  }
+
+  if (password.length > 8) {
+    conditions = conditions + 1;
+  }
+  if (password.match(/[a-z]/g)) {
+    conditions = conditions + 1;
+  }
+
+  if (password.match(/[A-Z]/g)) {
+    conditions = conditions + 1;
+  }
+  if (password.match(/\W/g)) {
+    conditions = conditions + 1;
+  }
+  if (password.match(/[0-9]/g)) {
+    conditions = conditions + 1;
+  }
+  console.log(conditions);
+
+  if (conditions >= 6) {
+    return "strong";
+  } else if (conditions >= 4) {
+    return "medium";
+  } else if (conditions >= 3) {
+    return "weak";
+  } else {
+    return "invalid";
+  }
+}
+
+console.log(passwordStrength("L@idobeats75"));
+
+// module.exports = { passwordIsValid, passwordStrength };
