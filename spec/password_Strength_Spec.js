c602876e085d19b523d708c4cb7ff735871f7f6f
Nkosi97 <nkosinathisiyeni97@gmail.com>
2021-02-02 17:54:19 +0200

    edited passwordStrength tests

--- spec/password_Strength_Spec.js
+++ spec/password_Strength_Spec.js
@@ -2,19 +2,19 @@ const { passwordStrength } = require("../src/password_Checker");
 
 
 describe("passwordStrength function", () => {
-  it(`should return strong`, () => {
+  it(`should return strong if number of conditions met >= 6`, () => {
     expect(passwordStrength("L@idobeats75")).toBe(`strong`)
   });
 
-  it(`should return medium`, () => {
+  it(`should return medium if number of conditions met >= 4`, () => {
     expect(passwordStrength("L@idobeats")).toBe(`medium`)
   });
 
-  it(`should return weak`, () => {
+  it(`should return weak if number of conditions met >= 3`, () => {
     expect(passwordStrength("Lido")).toBe(`weak`)
   });
 
-  it(`should return invalid`, () => {
+  it(`should return invalid if conditions 1 or 2 are not met`, () => {
     expect(passwordStrength("")).toBe(`invalid`)
     expect(passwordStrength("2016")).toBe(`invalid`)
   });
